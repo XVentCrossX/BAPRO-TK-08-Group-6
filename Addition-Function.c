30ae12a46caf880a83216724c748a2b6708ab1f7
XVentCrossX <landoakmalkane.muhammad@gmail.com>
2024-12-07 21:35:17 +0700

    Added changes into Addition-Function,c

--- Addition-Function.c
+++ Addition-Function.c
@@ -3,9 +3,9 @@
 int main() {
     float a, b, sum;
 
-    printf("Enter the first number (a): ");
+    printf("Enter the first number : ");
     scanf("%f", &a);
-    printf("Enter the second number (b): ");
+    printf("Enter the second number : ");
     scanf("%f", &b);
 
     sum = a + b;
